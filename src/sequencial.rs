9e0ed76ab90411966ac8513d25dc5cd390a35e09
hyranno <hyranno4pub@gmail.com>
2023-09-15 23:58:30 +0900

    add generalized sequence

--- src/sequencial.rs
+++ src/sequencial.rs
@@ -8,54 +8,82 @@ use bevy::ecs::entity::Entity;
 use super::{Node, NodeGen, NodeResult, complete_or_yield, nullable_access::NullableWorldAccess};
 
 
-pub type Sequence = SequencialAnd;
-pub struct SequencialAnd {
+pub struct SequenceWhile {
     nodes: Vec<Arc<dyn Node>>,
+    cond: Box<dyn Fn(NodeResult) -> bool + 'static + Send + Sync>,
+    complete_value: NodeResult,
 }
-impl SequencialAnd {
-    pub fn new(nodes: Vec<Arc<dyn Node>>,) -> Arc<Self> {
-        Arc::new(Self { nodes })
+impl SequenceWhile {
+    pub fn new(
+        nodes: Vec<Arc<dyn Node>>,
+        cond: impl Fn(NodeResult)->bool + 'static + Send + Sync,
+        complete_value: NodeResult
+    ) -> Arc<Self> {
+        Arc::new(Self { nodes, cond: Box::new(cond), complete_value })
     }
 }
-impl Node for SequencialAnd {
+impl Node for SequenceWhile {
     fn run(self: Arc<Self>, world: Arc<Mutex<NullableWorldAccess>>, entity: Entity) -> Box<dyn NodeGen> {
         let producer = |co| async move {
             for node in self.nodes.iter() {
                 let mut gen = node.clone().run(world.clone(), entity);
                 let node_result = complete_or_yield(&co, &mut gen).await;
-                match node_result {
-                    NodeResult::Success => {},
-                    _ => { return node_result; },
+                if !(self.cond)(node_result) {
+                    return node_result;
                 }
             }
-            NodeResult::Success
+            self.complete_value
         };
         Box::new(Gen::new(producer))
     }
 }
 
+pub type Sequence = SequencialAnd;
+pub struct SequencialAnd {
+    delegate: Arc<SequenceWhile>,
+}
+impl SequencialAnd {
+    pub fn new(nodes: Vec<Arc<dyn Node>>,) -> Arc<Self> {
+        Arc::new(Self {delegate: SequenceWhile::new(
+            nodes, |res| res==NodeResult::Success, NodeResult::Success
+        )})
+    }
+}
+impl Node for SequencialAnd {
+    fn run(self: Arc<Self>, world: Arc<Mutex<NullableWorldAccess>>, entity: Entity) -> Box<dyn NodeGen> {
+        self.delegate.clone().run(world, entity)
+    }
+}
+
 pub type Selector = SequencialOr;
 pub struct SequencialOr {
-    nodes: Vec<Arc<dyn Node>>,
+    delegate: Arc<SequenceWhile>,
 }
 impl SequencialOr {
     pub fn new(nodes: Vec<Arc<dyn Node>>,) -> Arc<Self> {
-        Arc::new(Self { nodes })
+        Arc::new(Self {delegate: SequenceWhile::new(
+            nodes, |res| res==NodeResult::Failure, NodeResult::Failure
+        )})
     }
 }
 impl Node for SequencialOr {
     fn run(self: Arc<Self>, world: Arc<Mutex<NullableWorldAccess>>, entity: Entity) -> Box<dyn NodeGen> {
-        let producer = |co| async move {
-            for node in self.nodes.iter() {
-                let mut gen = node.clone().run(world.clone(), entity);
-                let node_result = complete_or_yield(&co, &mut gen).await;
-                match node_result {
-                    NodeResult::Failure => {},
-                    _ => { return node_result; },
-                }
-            }
-            NodeResult::Failure
-        };
-        Box::new(Gen::new(producer))
+        self.delegate.clone().run(world, entity)
+    }
+}
+
+pub struct ForcedSequence {
+    delegate: Arc<SequenceWhile>,
+}
+impl ForcedSequence {
+    pub fn new(nodes: Vec<Arc<dyn Node>>,) -> Arc<Self> {
+        Arc::new(Self {delegate: SequenceWhile::new(
+            nodes, |_| true, NodeResult::Success
+        )})
+    }
+}
+impl Node for ForcedSequence {
+    fn run(self: Arc<Self>, world: Arc<Mutex<NullableWorldAccess>>, entity: Entity) -> Box<dyn NodeGen> {
+        self.delegate.clone().run(world, entity)
     }
 }
